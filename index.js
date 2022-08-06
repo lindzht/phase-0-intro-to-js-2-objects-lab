a7c744eee7ccb66e3ef8c93c38d07ae2f000eefb
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-08-06 14:45:02 -0400

    commit tree 01d5cb52c3103809aec7089ea4ad2624b58a5fbc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Lindsay",
+    streetAddress: "167 Prospect"
+};
+
+function updateEmployeeWithKeyAndValue (object, key, value) {
+    return {...object, 
+        [key]: value
+    }
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key){
+    const newObject = {...object};
+    delete newObject[key]
+    return newObject;
+};
+
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key];
+    return object
+}
